ac081b523d8004bb0a9d856a6516fb31372c3720
zawchan960-tech <zawchan960@gmail.com>
2025-08-20 17:42:19 +0630

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,5 @@ const username=window.prompt("Enter your name")
 Click.addEventListener("click",event=>{
     event.target.style.backgroundColor="tomato"
     Click.textContent= `I love you,${username} 😆`
-    if(event.target.react.visibility==="visible"){
-        event.target.react.visible="hidden"
-    }
-    else{
-        event.target.visible="visible"
 
-    }
 })
