fa5670ff24a2b157b098757b06e4994e1b7ae0ff
zawchan960-tech <zawchan960@gmail.com>
2025-08-20 17:28:20 +0630

    gh

---
+++ index.js
@@ -0,0 +1,14 @@
+const Click =document.getElementById("Clickbtn")
+const username=window.prompt("Enter your name")
+const  react=document.getElementById("reactbtn")
+Click.addEventListener("click",event=>{
+    event.target.style.backgroundColor="tomato"
+    Click.textContent= `I love you,${username} 😆`
+    if(event.target.react.visibility==="visible"){
+        event.target.react.visible="hidden"
+    }
+    else{
+        event.target.visible="visible"
+
+    }
+})
